e019deb29ed69af437c7cc3fd9923130585fde67
Takamasa Oshikiri <mail@oshikiri.org>
2018-09-16 19:26:00 +0900

    Use FlatSpec style at FromSchemaSpec

--- spec/src/test/scala/FromSchemaSpec.scala
+++ spec/src/test/scala/FromSchemaSpec.scala
@@ -6,24 +6,24 @@ class FromSchemaAnnotationSpec extends FlatSpec with Matchers {
 
   // FIXME: https://github.com/vegas-viz/Vegas/issues/93
   "fromSchemaResource" should "be compiled" ignore {
-    assertCompiles("""
+    """
       @fromSchemaResource(
         path = "/spec/src/main/resources/vega-lite-schema.json",
         name = "Vega",
         outPath = Some("spec/target/scala-2.11/Spec.scala")
       )
       object SpecFromSchemaResource
-    """)
+    """ should compile
   }
 
   "fromSchemaURL" should "be compiled" in {
-    assertCompiles("""
+    """
       @fromSchemaURL(
         url = "https://vega.github.io/schema/vega-lite/v1.2.0.json",
         name = "Vega",
         outPath = Some("spec/target/scala-2.11/Spec.scala")
       )
       object SpecFromSchemaURL
-    """)
+    """ should compile
   }
 }
